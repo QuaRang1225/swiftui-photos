7cd8490211b885b65dbc8b349c6ab84566844042
QuaRang <dbduddnd1225@gmail.com>
2024-08-06 13:03:55 +0900

    프로젝트 추가

---
+++ SwiftUI_Photos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Photos
+//
+//  Created by 유영웅 on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Photos/SwiftUI_PhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PhotosApp.swift
+//  SwiftUI_Photos
+//
+//  Created by 유영웅 on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

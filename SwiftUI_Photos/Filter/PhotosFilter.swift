a3d636b46db4a7d83979f85d1f030720e791c52f
QuaRang <dbduddnd1225@gmail.com>
2024-08-09 04:09:16 +0900

    #1 - 앨범 및 즐겨찾기
    - 전체 항목,즐겨찾는 항목,앨범 등 옵션에 맞게 리스트를 보여주도록 구현하기로함
    - 전체/즐찾/나머지를 구분하기 위해 enum 생성

---
+++ SwiftUI_Photos/Filter/PhotosFilter.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosFilter.swift
+//  SwiftUI_Photos
+//
+//  Created by 유영웅 on 8/9/24.
+//
+
+import Foundation
+
+enum PhotosFilter:String,CaseIterable{
+    case all = "전체 항목"
+    case bookmark = "즐겨찾는 항목"
+    case other
+}

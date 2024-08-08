6342c49895c2fc10bfdf6e2eed551404dbc4fb4f
QuaRang <dbduddnd1225@gmail.com>
2024-08-09 00:52:25 +0900

    #1 - 임시적으로 ContentsView 사용 안함
    - 당분간은 PhotosUI를 루트뷰로 사용할 예정

--- SwiftUI_Photos/Core/Main/View/ContentView.swift
+++ SwiftUI_Photos/Core/Main/View/ContentView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var offsetX = CGFloat.zero
+    @State var offset = CGSize.zero
     @StateObject var vm = PhotoViewModel()
     var body: some View {
         NavigationView{
             PhotosView()
-                .environmentObject(vm)
+                
         }
+//        .offset(offset)
+//        .onChange(of: offsetX){ value in
+//            print(value)
+//            if value > 70{
+//                withAnimation {
+//                    offset.height = 200
+//                }
+//            }
+//            else if value < 300{
+//                withAnimation {
+//                    offset.height = .zero
+//                }
+//            }
+//        }
+        .environmentObject(vm)
     }
 }
 
